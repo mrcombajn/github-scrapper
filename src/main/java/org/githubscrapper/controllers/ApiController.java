3aebe082d522667d2686e9775120317ef72230b2
Mateusz <mateusz.ostro@o2.pl>
2024-02-18 01:07:08 +0100

    Created endpoint in REST controller

--- src/main/java/org/githubscrapper/controllers/ApiController.java
+++ src/main/java/org/githubscrapper/controllers/ApiController.java
@@ -1,4 +1,17 @@
 package org.githubscrapper.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class ApiController {
+
+    @GetMapping("/{user}")
+    String getUserRepositories(@PathVariable String userName) {
+        return null;
+    }
+    
 }
